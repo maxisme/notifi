8e7f93418a6d75010e8d1308a5683bc806ac7e8f
Max Mitchell <max@maxis.me>
2017-01-04 16:32:24 +0000

    add github ribbon

--- notifi.it/index.php
+++ notifi.it/index.php
@@ -2,6 +2,8 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" /><link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet' type='text/css'>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/github-fork-ribbon-css/0.2.0/gh-fork-ribbon.min.css" />
+
 <title>Notifi</title>
 <style>
 *{
@@ -28,6 +30,7 @@ i{
 </head>
 
 <body>
+<a class="github-fork-ribbon" href="https://github.com/maxisme/notifi" title="Fork me on GitHub">Fork me on GitHub</a>
 <h1>notifi</h1>
 A minimal app that sits in your MAC OSX menu bar. When opening you are given a key that uniquely identifys you. With this key you can send curl requests to receive alerts.<br /><br />
 <a href="download">download now</a>
