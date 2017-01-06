9f2d2623dd63898f8c9e2ce7225f4184ed7c18b5
Max Mitchell <max@maxis.me>
2017-01-06 16:43:18 +0000

    Add script to convert Xcode project to app and upload zip of app to github

---
+++ OSX App/build.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+app_path="/Users/maxmitch/Documents/notifi/OSX App/"
+zip_output="/Users/maxmitch/Documents/notifi/notifi.it/notifi.zip"
+project=$app_path"notifi.xcodeproj"
+plist=$app_path"buildOptions.plist"
+output=$app_path"tmp.xcarchive" 
+dev_team="3H49MXS325"
+
+#build
+xcodebuild -project "$project" -scheme notify -configuration Release clean archive -archivePath "$output" DEVELOPMENT_TEAM=$dev_team
+xcodebuild -exportArchive -archivePath "$output" -exportOptionsPlist "$plist" -exportPath "$app_path"
+
+#zip app
+cd "$app_path"
+zip -r -y "$zip_output" notifi.app
+
+#remove temp files
+rm -rf "notifi.app"
+rm -rf "$output"
+
+#commit
+git commit /Users/maxmitch/Documents/notifi/notifi.it/notifi.zip -m "Update OSX App - via build script"
+git status
+git push origin master
+
