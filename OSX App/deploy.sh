33ddea2b234fdb18f2e1f9c21db6297e4fd05a9d
Max Mitchell <max@maxis.me>
2017-01-20 22:40:19 +0000

    Make deploy.sh more dynamic for different projects

--- OSX App/deploy.sh
+++ OSX App/deploy.sh
@@ -1,34 +1,59 @@
 #!/bin/bash
-app_path="/Users/maxmitch/Documents/notifi/OSX App/"
-zip_output="/Users/maxmitch/Documents/notifi/notifi.it/notifi.zip"
-project=$app_path"notifi.xcodeproj"
-plist=$app_path"buildOptions.plist"
-output=$app_path"tmp.xcarchive" 
+
+#INITIAL VARIABLES THAT NEED TO BE CUSTOMISED
+project_name="notifi"
+project_path="/Users/maxmitch/Documents/notifi/OSX App/"
 dev_team="3H49MXS325"
+zip_project_output="/Users/maxmitch/Documents/notifi/notifi.it/notifi.zip"
 
-#build
-xcodebuild -project "$project" -scheme notify -configuration Release clean archive -archivePath "$output" DEVELOPMENT_TEAM=$dev_team
-xcodebuild -exportArchive -archivePath "$output" -exportOptionsPlist "$plist" -exportPath "$app_path"
+#NOT IMPORTANT INITIAL VARIABLES
+xcode_project=$project_path$project_name".xcodeproj"
+plist=$project_path"buildOptions.plist" 
+xcarchive=$project_path"tmp.xcarchive"
 
-#zip app
-cd "$app_path"
-zip -r -y "$zip_output" notifi.app
+#countdown function 
+function countDown {
+	secs=$((20))
+	while [ $secs -gt 0 ]; do
+	   echo -ne "Will exit in $secs\033[0K\r"
+	   sleep 1
+	   : $((secs--)) 
+	done
+	exit
+}
 
-#remove temp files
-rm -rf "notifi.app"
-rm -rf "$output"
+#create temp .plist file
+echo "<!DOCTYPE plist PUBLIC \"-//Apple//DTD PLIST 1.0//EN\" \"http://www.apple.com/DTDs/PropertyList-1.0.dtd\"><plist version=\"1.0\"><dict><key>method</key><string>developer-id</string><key>teamID</key><string>$dev_team</string></dict></plist>" > "$plist"
+
+#build project
+xcodebuild -project "$xcode_project" -scheme "$project_name" -configuration Release clean archive -archivePath "$xcarchive" DEVELOPMENT_TEAM=$dev_team
+
+#check if succeeded
+if [ $? -ne 0 ]; then 
+	echo -e "Error."
+	countDown
+fi
+
+xcodebuild -exportArchive -archivePath "$xcarchive" -exportOptionsPlist "$plist" -exportPath "$project_path"
+
+#check if succeeded
+if [ $? -ne 0 ]; then 
+	echo "Error with export."
+	countDown
+fi
+
+#remove temp files used in build
+rm -rf "$project_name.app" "$xcarchive" "$plist"
+
+#zip signed project
+cd "$project_path" || exit
+zip -r -y "$zip_project_output" "$project_name.app"
 
 #commit
-git commit $zip_output -m "Update OSX App - via build script"
+git commit "$zip_project_output" -m "Update OSX App - via build script"
 git push origin master
 
 #upload to website
-scp $zip_output root@185.117.22.245:/var/www/notifi.it/public_html/
+scp "$zip_project_output" root@185.117.22.245:/var/www/notifi.it/public_html/
 
-#analyze results
-secs=$((20))
-while [ $secs -gt 0 ]; do
-   echo -ne "Will close in $secs\033[0K\r"
-   sleep 1
-   : $((secs--))
-done
+countDown
