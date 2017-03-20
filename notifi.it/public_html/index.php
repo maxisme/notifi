f6bbae1209b82bb1b9fd48d84cb1e9dd37448a6f
Max Mitchell <max@maxis.me>
2017-03-20 14:24:44 +0000

    -

--- notifi.it/public_html/index.php
+++ notifi.it/public_html/index.php
@@ -1,7 +1,5 @@
 <?php
 session_start();
-error_reporting(E_ALL);
-ini_set("display_errors", 1);
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
@@ -16,12 +14,13 @@ ini_set("display_errors", 1);
 	<link rel="shortcut icon" href="/favicons/favicon.ico">
 	<meta name="msapplication-config" content="/favicons/browserconfig.xml">
 	<meta name="theme-color" content="#ffffff">
+    <meta name="og:image" content="https://notifi.it/logo.png" />
 	<!-- meta stuff -->
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
 	<meta name="description" content="A minimal notification app for your Mac">
 	<meta name="keywords" content="OSX, Mac, Notify, Notifi, notifi, notification, app">
 	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0"/>
-	<!-- google fonts --> 
+	<!-- google fonts -->
 	<link href='https://fonts.googleapis.com/css?family=Muli' rel='stylesheet' type='text/css'>
 	<link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet' type='text/css'>
 	<!-- google recaptcha -->
@@ -34,7 +33,7 @@ ini_set("display_errors", 1);
 	<script type="text/javascript" src="https://maxis.me/template/scripts/jquery.easing.min.js"></script>
 	<script type="text/javascript" src="https://maxis.me/template/scripts/script.js"></script>
 
-	<title>Notifi</title>
+	<title>notifi</title>
 </head>
 
 <body>
@@ -45,38 +44,38 @@ ini_set("display_errors", 1);
 			<h1>notifi</h1>
 			<div id="navMenu">
 				<a class="page-scroll" href="#howitworks">How It Works</a> | <a class="page-scroll" href="#contact">Contact</a> | <a class="page-scroll" href="#tsandcs">Ts & Cs</a>
-			
+
 			</div><br/><br/><br/>
 			<a id="down" href="download">download</a><br/><br/>
 		</div>
-	</page> 
- 
+	</page>
+
 	<!-- HOW IT WORKS -->
 	<page id="howitworks">
 		<h2>How It Works</h2>
 		<div>
 			<p class="info">
-				Notifi is a <strong>FREE</strong>, minimal <strong>Push Notification app</strong> that sits in your MAC OSX menu bar. When you open the app for the first time you are given a key that uniquely identifys you. <em>Blured in the image below:</em>
+				Notifi is a <strong>FREE</strong>, minimal <strong>Push Notification app</strong> that sits in your Mac menu bar. When you open the app for the first time you are given a key that uniquely identifies you. <em>Blurred in the image below:</em>
 			</p>
 			<p>
 				<img style="zoom:98.6%" src="https://github.com/maxisme/notifi/raw/master/Screen%20Shots/Menubar.png">
 			</p>
 			<p class="info">
-				With this key you can send HTTP requests to receive notifications on your mac.
+				With this key you can send <a target="_blank" href="https://github.com/maxisme/notifi#http-request-examples">HTTP requests (curl)</a> to receive notifications on your mac.
 			</p>
 			<p>
 				<img style="zoom:98.6%"  src="https://github.com/maxisme/notifi/raw/master/Screen%20Shots/Window.png">
 			</p>
 			<h3>Okay I have downloaded it. Now what?</h3>
 			<p class="info">
-				First click on Notifi which will appear on your menu bar at the top of your Mac. Here you can open your notifications and get your unique <strong>Credentials</strong>.<br/><br/> Send a notification with your desired programming language or through the terminal - <a target="_blank" href="https://github.com/maxisme/notifi#http-request-examples">See examples</a>.
+				First click on notifi which will appear on your menu bar at the top of your Mac. Here you can open your notifications and get your unique <strong>Credentials</strong>.<br/><br/> Send a notification with your desired programming language or through the terminal - <a target="_blank" href="https://github.com/maxisme/notifi#http-request-examples">See examples</a>.
 				<p>
 					<img style="zoom:98.6%" src="https://raw.githubusercontent.com/maxisme/notifi/master/Screen%20Shots/Notification.png">
 				</p>
 			</p>
 			<h3>Want to know more?</h3>
 			<p class="info">
-				Notifi is open source on <a target="_blank" href="https://github.com/maxisme/notifi">GitHub</a>.
+				notifi is open source on <a target="_blank" href="https://github.com/maxisme/notifi">GitHub</a>.
 			</p>
 		</div>
 	</page>
@@ -118,9 +117,9 @@ ini_set("display_errors", 1);
 	<!-- Ts & Cs -->
 	<page id="tsandcs">
 		<h2>Ts & Cs</h2>
-	
+
 		<div class="info">
-			By using the Notifi application you are agreeing to these terms:<br/><br/> Use of the Service is at your own risk. The Service is provided on an "AS IS" and "AS AVAILABLE" basis without any representation or endorsement made and without warranty of any kind whether expressed or implied, including but not limited to the implied warranties of satisfactory quality, fitness for a particular purpose, non-infringement, compatibility, security and accuracy.<br/><br/>To the extent permitted by law, the Service, will not be liable for any indirect or consequential loss or damage whatsoever (including without limitation, loss of business, opportunity, data, profits) arising out of or in connection with the use of the Service.
+			By using the notifi application you are agreeing to these terms:<br/><br/> Use of the Service is at your own risk. The Service is provided on an "AS IS" and "AS AVAILABLE" basis without any representation or endorsement made and without warranty of any kind whether expressed or implied, including but not limited to the implied warranties of satisfactory quality, fitness for a particular purpose, non-infringement, compatibility, security and accuracy.<br/><br/>To the extent permitted by law, the Service, will not be liable for any indirect or consequential loss or damage whatsoever (including without limitation, loss of business, opportunity, data, profits) arising out of or in connection with the use of the Service.
 			<p><sub>icon from flaticon.com</sub></p>
 		</div>
 	</page>
