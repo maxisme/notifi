b6f370fe73579db6304f1b510aae9a5f7b3243bd
Max Mitchell <max@maxis.me>
2017-03-23 13:57:00 +0000

    add image size limitation and error messages

--- notifi.it/public_html/api.php
+++ notifi.it/public_html/api.php
@@ -8,6 +8,7 @@ $db_pass = trim(file_get_contents(dirname(__DIR__)."/db.pass"));
 $db_user = trim(file_get_contents(dirname(__DIR__)."/db.user"));
 $key = trim(file_get_contents(dirname(__DIR__)."/encryption.key"));
 $char_limit = 20000;
+$img_limit = 10;
 $con = connect();
 
 //post data
@@ -15,18 +16,18 @@ $credentials = trim(clean(mysqli_real_escape_string($con,$_POST['credentials']))
 $title = trim(mysqli_real_escape_string($con,$_POST['title']));
 if (isset($_POST["message"]))
 	$message = trim(mysqli_real_escape_string($con,$_POST['message']));
-if (isset($_POST["img"]))
-	$imageURL = trim(mysqli_real_escape_string($con,$_POST['img']));
+if (isset($_POST["image"]))
+	$imageURL = trim(mysqli_real_escape_string($con,$_POST['image']));
 if (isset($_POST["link"]))
 	$link = trim(mysqli_real_escape_string($con,$_POST['link']));
 
 //validation
 if(empty($credentials) || strlen($credentials) != 25){
-	die("invalid credentials\n");
+	die("Invalid credentials\n");
 } 
 
 if(empty($title)){
-	die("No title\n"); 
+	die("You must enter a title\n");
 }else if(strlen($title) > $char_limit){
 	die("Title too long! Must be less than $char_limit charachters\n");
 }
@@ -41,15 +42,20 @@ if(empty($imageURL)){
 	$imageURL = " "; 
 }else{
 	if (!@getimagesize($imageURL)) {
-		echo "$imageURL is not a valid image. Sent without!\n";
-		$imageURL = " "; 
-	} 
+		echo "$imageURL is not a valid image. Sent without image!\n";
+	}else {
+        $img_size = get_headers($imageURL, 1)["Content-Length"] / 1048576; //bytes to mb
+	    if($img_size > $img_limit) {
+            echo "Image too large. Must be under ${img_limit}MB. It is ${img_size}MB. Sent without image!\n";
+            $imageURL = " ";
+        }
+    }
 } 
 
 if(empty($link)){ 
 	$link = " "; 
 }else if (filter_var($link, FILTER_VALIDATE_URL) === FALSE) {
-    echo "$link is not a valid link. Sent without\n";
+    echo "$link is not a valid link. Sent without link!\n";
 	$link = " "; 
 }
 
@@ -61,7 +67,6 @@ if(isBruteForce($credentials)){
 if(!userExists(myHash($credentials))){
 	die();
 }
-
 
 $insert_notification_query = mysqli_query($con, "INSERT INTO notifications (credentials, title, message, image, link) VALUES (
 	'".myHash($credentials)."', 
