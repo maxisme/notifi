596997fab61c6b068699241844d56b6f3ff288c8
Maximilian Mitchell <max@max.me.uk>
2021-04-16 22:09:12 +0100

    use mv in afer-ps-save.sh

--- logo/after-ps-save.sh
+++ logo/after-ps-save.sh
@@ -1,15 +1,15 @@
 #mac
-cp -r Your_App_Icon-assets/mac.appiconset ../macos/Runner/Assets.xcassets/AppIcon.appiconset
-cp Your_App_Icon-assets/error_menu.png ../macos/Runner/Assets.xcassets/menu_error_icon.imageset/
-cp Your_App_Icon-assets/menu.png ../macos/Runner/Assets.xcassets/menu_icon.imageset/
-cp Your_App_Icon-assets/menu_red.png ../macos/Runner/Assets.xcassets/red_menu_icon.imageset/
+mv Your_App_Icon-assets/mac.appiconset/* ../macos/Runner/Assets.xcassets/AppIcon.appiconset/
+mv Your_App_Icon-assets/error_menu.png ../macos/Runner/Assets.xcassets/menu_error_icon.imageset/
+mv Your_App_Icon-assets/menu.png ../macos/Runner/Assets.xcassets/menu_icon.imageset/
+mv Your_App_Icon-assets/menu_red.png ../macos/Runner/Assets.xcassets/red_menu_icon.imageset/
 
 ##ios
-#cp -r Your_App_Icon-assets/AppIcon.appiconset ../ios/Runner/Assets.xcassets/AppIcon.appiconset
+#mv -r Your_App_Icon-assets/AppIcon.appiconset ../ios/Runner/Assets.xcassets/AppIcon.appiconset
 
 ## flutter
-cp Your_App_Icon-assets/sad.png ../images/
-cp Your_App_Icon-assets/bell.png ../images/
+mv Your_App_Icon-assets/sad.png ../images/
+mv Your_App_Icon-assets/bell.png ../images/
 
 
 # TODO splash
