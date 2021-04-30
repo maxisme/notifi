6459a868466ddc4c8cd794115fb402f86175a61e
Maximilian Mitchell <max@max.me.uk>
2021-04-30 20:37:32 +0100

    .

--- .github/workflows/scripts/notarize.sh
+++ .github/workflows/scripts/notarize.sh
@@ -4,13 +4,19 @@
 
 echo "verify..."
 spctl -a -vv build/macos/Build/Products/Release/notifi.app
-spctl -a -vv -vvvv "$DMG_PATH"
+spctl -a -vv "$DMG_PATH"
 
 echo "notarizing..."
-xcrun altool -t osx --primary-bundle-id it.notifi.notifi --output-format json --notarize-app -f "$DMG_PATH" --username "$APPLE_USERNAME" --password "$APPLE_PASSWORD"
+notarize=$(xcrun altool -t osx --primary-bundle-id it.notifi.notifi --output-format json --notarize-app -f "$DMG_PATH" --username "$APPLE_USERNAME" --password "$APPLE_PASSWORD")
+uuid=$(echo "$notarize" | python3 -c "import sys, json; print(json.load(sys.stdin)['notarization-upload']['RequestUUID'])")
+echo "$uuid"
 
-echo "verify again ..."
-spctl -a -vv -vvvv "$DMG_PATH"
+sleep 60
+
+check=$(xcrun altool --notarization-info "$uuid" --output-format json --username "$APPLE_USERNAME" --password "$APPLE_PASSWORD")
+echo $check
+status=$(echo "$check" | python3 -c "import sys, json; print(json.load(sys.stdin)['Status'])")
+echo $status
 
 echo "staple..."
 xcrun stapler staple -v "$DMG_PATH"
