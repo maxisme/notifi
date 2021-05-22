8264e1b780f87866dd060ea8432ae91a73e0f63e
Maximilian Mitchell <max@max.me.uk>
2021-05-22 15:17:45 +0100

    add pull down refresh web socket resolves #71

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -4,10 +4,14 @@ import 'package:notifi/notifications/notifications_table.dart';
 import 'package:notifi/notifications/notifis.dart';
 import 'package:notifi/screens/utils/alert.dart';
 import 'package:notifi/screens/utils/appbar_title.dart';
+import 'package:notifi/user.dart';
 import 'package:notifi/utils/icons.dart';
 import 'package:provider/provider.dart';
+import 'package:pull_to_refresh/pull_to_refresh.dart';
 
 class HomeScreen extends StatelessWidget {
+  final RefreshController _refreshController = RefreshController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,7 +26,32 @@ class HomeScreen extends StatelessWidget {
                 Navigator.pushNamed(context, '/settings');
               }),
         ),
-        body: const NotificationTable(),
+        body: SmartRefresher(
+            header: CustomHeader(
+              builder: (_, RefreshStatus mode) {
+                // if (mode == RefreshStatus.completed) {
+                //   _refreshController.refreshToIdle();
+                // }
+                return const SizedBox(
+                  height: 40.0,
+                  width: 40.0,
+                  child: Center(
+                    child: SizedBox(
+                        height: 20.0,
+                        width: 20.0,
+                        child: CircularProgressIndicator(
+                          strokeWidth: 1,
+                        )),
+                  ),
+                );
+              },
+            ),
+            controller: _refreshController,
+            onRefresh: () async {
+              _refreshController.refreshCompleted();
+              await Provider.of<User>(context, listen: false).initWSS();
+            },
+            child: const NotificationTable()),
         bottomNavigationBar: BottomAppBar(
           // ignore: prefer_const_literals_to_create_immutables
           child: SizedBox(
