6a004ae19df4b68992114ca7e6c247399063684e
Maximilian Mitchell <max@max.me.uk>
2021-05-26 20:04:56 +0100

    reformat scaffold app bar and bottom bar

--- lib/screens/utils/scaffold.dart
+++ lib/screens/utils/scaffold.dart
@@ -2,31 +2,34 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:notifi/notifications/notifis.dart';
 import 'package:notifi/screens/utils/animated_cnt.dart';
-import 'package:notifi/utils/icons.dart';
 import 'package:notifi/utils/pallete.dart';
 import 'package:provider/provider.dart';
 
-
 class MyScaffold extends StatelessWidget {
-  const MyScaffold({this.body, this.bottomNavigationBar});
+  const MyScaffold({this.leading, this.body, this.bottomNavigationBar});
 
+  final Widget leading;
   final Widget body;
   final Widget bottomNavigationBar;
 
   @override
   Widget build(BuildContext context) {
     double paddingTop = 0;
-    if(MediaQuery.of(context).padding.top > 0){
-      paddingTop = MediaQuery.of(context).padding.top - 20.0;
+    if (MediaQuery.of(context).padding.top > 0) {
+      paddingTop = MediaQuery.of(context).padding.top - 20;
     }
+    double paddingBottom = 0;
+    if (MediaQuery.of(context).padding.bottom > 0) {
+      paddingBottom = MediaQuery.of(context).padding.bottom / 2;
+    }
+
     return MediaQuery.removePadding(
         removeTop: MediaQuery.of(context).padding.top > 0,
         removeBottom: true,
         context: context,
         child: Container(
-          color: MyColour.white,
-            padding:
-                EdgeInsets.only(top: paddingTop),
+            color: MyColour.white,
+            padding: EdgeInsets.only(top: paddingTop, bottom: paddingBottom),
             child: Scaffold(
                 appBar: AppBar(
                   automaticallyImplyLeading: false,
@@ -46,23 +49,20 @@ class MyScaffold extends StatelessWidget {
                                   .scrollToTop();
                             },
                             child: Image.asset('images/bell.png',
-                                height: 60, filterQuality: FilterQuality.medium),
+                                height: 70,
+                                filterQuality: FilterQuality.medium),
                           ),
                         ),
                         Positioned(
-                          top: 7,
+                          top: 9.6,
                           child: Container(
-                              padding: const EdgeInsets.only(left: 17.0),
+                              padding: const EdgeInsets.only(left: 20),
                               child: const AnimatedCnt()),
                         ),
                       ],
                     ),
                   ),
-                  leading: IconButton(
-                      icon: const Icon(Akaricons.gear),
-                      onPressed: () async {
-                        Navigator.pushNamed(context, '/settings');
-                      }),
+                  leading: leading,
                 ),
                 body: body,
                 bottomNavigationBar: bottomNavigationBar)));
