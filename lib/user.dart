3ead9d1e1d0d65ed99b1b17bf7a76a0984691d80
Maximilian Mitchell <max@max.me.uk>
2021-09-12 12:31:16 +0100

    Fix integration tests

--- lib/user.dart
+++ lib/user.dart
@@ -81,7 +81,7 @@ class User with ChangeNotifier {
           postData['firebase_token'] =
               await FirebaseMessaging.instance.getToken();
         } catch (e) {
-          L.e(e);
+          L.e(e.toString());
         }
       }
       L.w('Replacing credentials: ${_user.credentials}');
