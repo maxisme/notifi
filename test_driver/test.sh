59dabaf5137cacf9eb32af258ef3bdd2c33e7bcb
Maximilian Mitchell <max@max.me.uk>
2021-09-15 20:24:34 +0100

    fix actions/checkout@v2 checkout

---
+++ test_driver/test.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+device=$1
+branch=$2
+if [ -n "$2" ]; then
+  if [[ "$branch" == "master" ]]; then
+    echo "$3" | base64 --decode >.env
+  else
+    echo "$4" | base64 --decode >.env
+  fi
+fi
+
+if [[ $device == *"iPhone"* ]]; then
+  if ! xcrun simctl boot "$device"; then
+    exit 1
+  fi
+fi
+
+flutter drive --target=test_driver/app.dart -d "$device"
