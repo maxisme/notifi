0cd90632c92e6d05289dea40cb41ae9311cb2bf6
Maximilian Mitchell <max@max.me.uk>
2021-12-10 15:18:26 +0000

    remove unnecessary_import's

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,7 +1,6 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:notifi/notifications/notifications_table.dart';
 import 'package:notifi/notifications/notifis.dart';
 import 'package:notifi/screens/utils/alert.dart';
